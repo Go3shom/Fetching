464921306d2dd06b0a14773b5fb49886b6ac34af
Go3shom <shadi.muhammad001@gmail.com>
2019-03-11 21:02:50 +0200

    Last Index Update

--- index.php
+++ index.php
@@ -5,33 +5,28 @@
     <title>Fetch</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <style>
+        /* 010101 | 69779B | ACDBDF | F0ECE2 */
+        
         table { border-collapse: collapse; width: 85%; color: #010101; font-family: 'Roboto'; font-size: 20px; text-align: center; margin: 30px auto; }
         th {  padding: 10px; background-color: #69779b;}
-        th > a { color: #f0ece2; text-decoration: none; }
+        th > a { color: #F0ECE2; text-decoration: none; }
         th > a:hover { color: #010101; }
-        tr:nth-child(even) { background-color: #f0ece2; }
+        tr:nth-child(even) { background-color: #F0ECE2; }
     </style>
 </head>
 <body>
     <?php
-        $server = "localhost";
-        $user = "root";
-        $password = "";
-        $dbName = "targetDb";
 
-        $conn = new mysqli( $server, $user, $password, $dbName );
+        $conn = new mysqli( "localhost", "root", "", "targetDb" );
 
-        if ($conn->connect_error ) {
+        if ( $conn->connect_error ) {
             die( "Connection Failed: " . $conn->connect_errno );
         }
 
+        // $sql = construct_sql( $sql, $sort_field, $sort_order );
         $sql = "SELECT ClientID, ClientName, ClientAddress FROM Clients";
-
-        $sort = " ORDER BY ClientName DESC";
-
-        $concat = $sql . " " . $sort . " ";
-
-        $result = $conn->query( $concat );
+     
+        $result = $conn->query( $sql );
 
         echo "<table><thead>";
             display_query_header( $result );
@@ -42,24 +37,50 @@
         $conn->close();
 
         /********************************************************/ 
+        function show_report( $conn ) {
+            if ( isset( $_GET[ "sort_field" ] ) ) {
+                $sort_field = $_GET[ "sort_field" ];
+                $sort_order = $_GET[ "sort_order" ];
+                
+                $sort_order == 'DESC' ? $sort_order = 'ASC' : $sort_order = 'DESC';
+            } else {
+                $sort_order = 'ASC';
+                $sort_field = 1;
+            }
+            $sql = construct_sql( $sql, $sort_field, $sort_order );
+            display_query( $conn, $sql, $sort_order );
+        }
+        /********************************************************/ 
+        function construct_sql( $sql, $sort_field, $sort_order ) {
+            $sql = "SELECT ClientID, ClientName, ClientAddress FROM Clients";
+            if ( $sort_field ) {
+                $sql .= " ORDER BY " . $sort_field;
+                $sql .= " " . $sort_order;
+            }
+            return ( $sql );
+        }
+        /********************************************************/         
+        function display_query( $conn, $sql, $sort_order ) {
+
+        }
+        /********************************************************/ 
         function display_query_header( $result ) {
             $field_cnt = $result->field_count;
             for ( $i = 0; $i < $field_cnt; $i++ ) { 
                 $field = $result->fetch_field();
-                echo "<th><a href = '$i'>" . $field->name . "</a></th>";
-
-                $url = $_GET[ " " . $i . " sort-order" ] ;
+                $j = $i + 1;
+                echo "<th><a href = ' ?sort_field=$j&sort_order=sort_order' >" . $field->name . "</a></th>";
             }
-        }
+        } 
         /********************************************************/ 
         function display_query_rows( $result ) {
             $field_cnt = $result->field_count;
             if ( $result->num_rows > 0 ) {
                 while ( $row = $result->fetch_array() ) {
                     echo "<tr>";
-                    for ( $j = 0; $j < $field_cnt; $j++ ) { 
-                        echo "<td>" . $row[ $j ] . "</td>";
-                    }
+                        for ( $j = 0; $j < $field_cnt; $j++ ) { 
+                            echo "<td>" . $row[ $j ] . "</td>";
+                        }
                     echo "</tr>";
                 }
             } else {
